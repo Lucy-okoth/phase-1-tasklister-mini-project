02f7e62746af67c7620d92113c00deafca139db9
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-08-23 08:56:24 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildEvents(e.target.newtaskdescription.value)
+    form.reset()
+  })
+}); 
+
+function buildEvents (todo) {
+  let p = document.createElement('p')
+  p.textContent = `${todo}  `
+  let btn = document.createElement('button')
+  btn.textContent= 'x'
+  btn.addEventListener('click', handleDelete)
+  document.querySelector('ul').appendChild(p)
+  p.appendChild(btn)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
